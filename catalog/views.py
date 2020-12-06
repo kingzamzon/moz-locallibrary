eb27c3e3bea97ee7d5e7e5bc4abe4b20fe7c5307
Adesanoye Samson <adesanoye.samson@yahoo.com>
2020-12-06 21:20:19 +0100

    implement author section with the list and details

--- catalog/views.py
+++ catalog/views.py
@@ -29,6 +29,13 @@ from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 2
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
